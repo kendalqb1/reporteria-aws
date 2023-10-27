169a65f5973e475f14db073c61ad5e9f2ec1f3b0
Kendal Quesada <kendalqb1@gmail.com>
2023-10-27 14:44:21 -0600

    Added modal view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState, useEffect } from 'react';
 import * as XLSX from 'xlsx';
-import { BsArrowRepeat, BsFileExcel } from 'react-icons/bs'; // Importar iconos
+import { BsArrowRepeat } from 'react-icons/bs';
+import { PiMicrosoftExcelLogo } from 'react-icons/pi';
+import { BiMessageAltDetail } from 'react-icons/bi' // Iconos
 import './App.css'
+import Swal from 'sweetalert2'
 
 
 export const App = () => {
   const [data, setData] = useState([]); // Almacenar los datos del endpoint
   const [isLoading, setIsLoading] = useState(false); // Estado para mostrar el mensaje de carga
+
+  const printModal = (item) => {
+    Swal.fire({
+      icon: 'message',
+      title: item.messageId,
+      text: item.message,
+      confirmButtonColor: '#3085d6',
+    });
+  }
+
 
   const exportToExcel = () => {
     const ws = XLSX.utils.json_to_sheet(data);
@@ -56,7 +69,7 @@ export const App = () => {
         onClick={exportToExcel}
       >
         <div className="flex justify-center items-center">
-          <BsFileExcel />
+          <PiMicrosoftExcelLogo />
           <p className="ml-2">Exportar</p>
         </div>
       </button>
@@ -69,7 +82,7 @@ export const App = () => {
               <th className="bg-gray-100 text-gray-600">Sender</th>
               <th className="bg-gray-100 text-gray-600">Msisdn</th>
               <th className="bg-gray-100 text-gray-600">Datetime</th>
-              <th className="bg-gray-100 text-gray-600">Message</th>
+              <th className="bg-gray-100 text-gray-600">Accion</th>
             </tr>
           </thead>
           <tbody>
@@ -81,7 +94,17 @@ export const App = () => {
                     <td className="border border-gray-300 p-2">{item.sender}</td>
                     <td className="border border-gray-300 p-2">{item.msisdn}</td>
                     <td className="border border-gray-300 p-2">{item.datetime}</td>
-                    <td className="border border-gray-300 p-2">{item.message}</td>
+                    <td className="border border-gray-300 p-2 text-center">
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => printModal(item)}
+                      >
+                        <div className="flex justify-center items-center">
+                          <BiMessageAltDetail />
+                          <p className="ml-2">Ver Mensaje</p>
+                        </div>
+                      </button>
+                    </td>
                   </tr>
                 ))}
           </tbody>
